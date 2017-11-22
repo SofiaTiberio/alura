45a5b23bc6eca57ab33e069b96dd2319e1ee2fe8
Sofia Tiberio <sofia-tiberio@hotmail.com>
2017-11-22 16:49:26 -0200

    Aula 1 à 8

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,36 +1,48 @@
-angular.module('projeto')
-    .controller('FotoController', function($scope, $routeParams, $location, FotoService) {
+angular.module('alurapic')
+.controller('FotoController', function($scope, $http, $routeParams) {
 
-        $scope.foto = {};
-        $scope.mensagem = '';
-    
-        if($routeParams.fotoId) {
-            FotoService.get({fotoId: $routeParams.fotoId}, function(retorno) {
-                 $scope.foto = retorno;
-            });
+    $scope.foto = {};
+    $scope.mensagem = '';
+
+    if($routeParams.fotoId) {
+        $http.get('/v1/fotos/' + $routeParams.fotoId)
+        .success(function(foto) {
+            $scope.foto = foto;
+        })
+        .error(function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível obter a foto'
+        });
+    }
+
+    $scope.submeter = function() {
+
+        if ($scope.formulario.$valid) {
+
+            if($routeParams.fotoId) {
+
+                $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
+                .success(function() {
+                    $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' foi alterada';
+
+                })
+                .error(function(erro) {
+                    console.log(erro);
+                    $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
+                });
+
+            } else {                
+                $http.post('/v1/fotos', $scope.foto)
+                .success(function() {
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto cadastrada com sucesso';
+                })
+                .error(function(erro) {
+                    console.log(erro);
+                    $scope.mensagem = 'Não foi possível cadastrar a foto';
+                })
+            }
         }
+    };
 
-        $scope.submeter = function() {
-           if($scope.formulario.$valid) {
-                if(!$routeParams.fotoId) {
-                    
-                    FotoService.save($scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Salvo com sucesso';
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível salvar';
-                    });
-
-                } else {
-                    FotoService.update({fotoId : $scope.foto._id}, $scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Alterado com sucesso';                        
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });                    
-                }
-           } 
-        };
-    });
+});
